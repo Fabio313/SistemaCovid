65a1ee9b586ed6146fe8be5360f4dfdc18b11a91
Fabio Zenatti Ferrenha <fabiozf3108@gmail.com>
2022-03-02 09:20:33 -0300

    Adicionar arquivos de projeto.

---
+++ FilaNaoPreferencial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaHospital
+{
+    internal class FilaNaoPreferencial
+    {
+        public Pessoa Head { get; set; }
+        public Pessoa Tail { get; set; }
+
+
+        public FilaNaoPreferencial()
+        {
+            Head = Tail = null;
+        }
+        public void Push(Pessoa naopreferencial)
+        {
+            if (Vazio())
+            {
+                Head = Tail = naopreferencial;
+            }
+            else
+            {
+                naopreferencial.Before = Tail;
+                Tail.Next = naopreferencial;
+                Tail = naopreferencial;
+            }
+        }
+        public bool Vazio()
+        {
+            if (Head == null && Tail == null)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ Hospital.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaHospital
+{
+    internal class Hospital
+    {
+        public int Nleitos { get; set; }
+        public FilaPessoa FilaPessoas { get; set; }
+    }
+}
